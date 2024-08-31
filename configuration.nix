2c26fdc410714a41b00bda51da9438a669fc7955
Travis Kinney <travisty-@users.noreply.github.com>
2024-08-31 15:41:26 -0700

    Rename 'services.xserver.displayManager.autoLogin'

--- configuration.nix
+++ configuration.nix
@@ -96,8 +96,8 @@
   };
 
   # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "travis";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "travis";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
