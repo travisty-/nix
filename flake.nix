94d693721f9348216ead99b54c5c5208d0797c11
Travis Kinney <travisty-@users.noreply.github.com>
2024-09-05 18:19:34 -0700

    Update modules to use default.nix

--- flake.nix
+++ flake.nix
@@ -14,13 +14,13 @@
     homeConfigurations = {
       travis = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [./home/home.nix];
+        modules = [./home];
       };
     };
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [./hosts/nixos/configuration.nix];
+        modules = [./hosts/nixos];
       };
     };
   };
--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -8,7 +8,7 @@
 }: {
   imports = [
     # Include the results of the hardware scan.
-    ./hardware-configuration.nix
+    ./hardware.nix
   ];
 
   # Enable support for Nix flakes.
