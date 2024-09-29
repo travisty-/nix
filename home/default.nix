8bb466d1e2097ff95c2c36cba18bd90e58bc0c06
Travis Kinney <travisty-@users.noreply.github.com>
2024-09-28 20:36:54 -0700

    Move modules into categorized folders

--- home/default.nix
+++ home/default.nix
@@ -4,14 +4,14 @@
   ...
 }: {
   imports = [
-    ./alacritty.nix
-    ./eza.nix
-    ./fd.nix
-    ./fzf.nix
-    ./git.nix
-    ./ripgrep.nix
-    ./zoxide.nix
-    ./zsh.nix
+    ../modules/home/alacritty.nix
+    ../modules/home/eza.nix
+    ../modules/home/fd.nix
+    ../modules/home/fzf.nix
+    ../modules/home/git.nix
+    ../modules/home/ripgrep.nix
+    ../modules/home/zoxide.nix
+    ../modules/home/zsh.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should manage.
