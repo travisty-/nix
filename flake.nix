d5dde1fdf3a61e340ab08883103d9b5b5bc58253
Travis Kinney <travisty-@users.noreply.github.com>
2024-09-28 20:39:52 -0700

    Move users into categorized folders

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     homeConfigurations = {
       travis = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [./home];
+        modules = [./home/travis];
       };
     };
     nixosConfigurations = {
