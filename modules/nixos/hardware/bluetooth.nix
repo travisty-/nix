24732ab3342a5884d70a601043f9f99d570a3257
Travis Kinney <travisty-@users.noreply.github.com>
2025-01-11 13:31:40 -0800

    Add bluetooth hardware to system modules

---
+++ modules/nixos/hardware/bluetooth.nix
@@ -0,0 +1,12 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  hardware.bluetooth = {
+    enable = true;
+
+    # Show battery charge of connected Bluetooth devices.
+    settings.general.experimental = true;
+  };
+}
