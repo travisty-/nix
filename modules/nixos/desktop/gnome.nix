8d81db63932abf92ebdeae70d6cdc6624fc5bc8e
Travis Kinney <travisty-@users.noreply.github.com>
2025-01-24 21:26:08 -0800

    Add GNOME configuration to system modules

---
+++ modules/nixos/desktop/gnome.nix
@@ -0,0 +1,16 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+}
